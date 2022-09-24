071b2f061a6f42ed9574b790a9ece1d99f1c0631
Sebastian Ware <sebastian@urbantalk.se>
2022-09-24 19:46:18 +0200

    Base scripts and config

---
+++ README.md
@@ -0,0 +1,13 @@
+# Streamix Graphics Package Templates
+
+Streamix Panel is a graphics overlay software for the ATEM Mini series of video mixers. Graphics are created using packages that can be imported to your project.
+
+1. Run the package creation script
+
+```sh
+source <(curl -s https://raw.githubusercontent.com/jhsware/streamix-package-templates/master/bin/create.sh)
+```
+
+2. Follow the instructions
+
+3. Start coding!
---
+++ bin/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+BASEDIR=$(dirname "$0")
+IFS_DEFAULT=$IFS
+
+if [ $BASEDIR != "." ]
+then
+    echo "You should run the npm script: 'npm run release'"
+    exit 1
+fi
+
+echo "*********************************"
+echo "** Create release of templates **"
+echo "*********************************"
+
+declare -a TEMPLATES=(
+    "graphics-template graphicsOverlayTemplate.tgz"
+    "stinger-template stingerTemplate.tgz"
+)
+
+for val in "${TEMPLATES[@]}"; do
+    tmp=($val)
+    src=${tmp[0]}
+    dest=${tmp[1]}
+    # echo "- $src $dest"
+    echo "${GREEN}$src:${NC} Releasing template as release/$dest"
+    cp docs/*.md templates/$src
+    tar -cvzf release/$dest templates/$src
+    rm templates/$src/*.md
+done
+
+echo "${GREEN}DONE!${NC}"
+echo
