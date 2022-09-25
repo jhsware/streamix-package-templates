81befbbc198aeaaad491b4164c7fef6aeb77b2c1
Sebastian Ware <sebastian@urbantalk.se>
2022-09-25 21:21:20 +0200

    Remove create.sh if it exists

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+if [ -f create.sh ]
+then
+  rm create.sh
+fi
+
 curl -s https://raw.githubusercontent.com/jhsware/streamix-package-templates/master/bin/create.sh -o create.sh
 chmod 755 create.sh
 echo -e "${GREEN}DONE! Template package creator installed!${NC}"
